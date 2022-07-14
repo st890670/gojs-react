06fd6575d164455bdc61b45f49e2cd400649e7a9
Jason <st890670@gmail.com>
2022-07-14 17:15:55 +0800

    build: initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+# gojs-react Example
+
+- This project is for investigating how the gojs-react implement.
+- Please refer to this [official document](https://gojs.net/latest/intro/react.html)
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import Chart from './Chart';
+import './style.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(<Chart />);
