232465556521fc895b0d36bfcb9e42d1a0320d5e
Jason <st890670@gmail.com>
2022-07-15 15:07:12 +0800

    feat: add wrapper component
    1. According to official document. We should use a shell componenet wrap the ReactDiagram component.

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,5 +1,4 @@
-import { useRef } from 'react';
-import { ReactDiagram } from 'gojs-react';
+import ChartWrapper from './ChartWrapper';
 import go from 'gojs';
 
 const MOCK_NODE_DATA = [
@@ -15,57 +14,21 @@ const MOCK_NODE_DATA = [
   },
 ] as Array<go.ObjectData>;
 
-const MOCK_LINK_DATA = [] as Array<go.ObjectData>;
+const MOCK_LINK_DATA = [
+  {
+    key: -1,
+    from: 0,
+    to: 1,
+  },
+] as Array<go.ObjectData>;
 
 function Chart() {
-  const ref = useRef(null);
-
-  function initDiagram() {
-    const $ = go.GraphObject.make;
-    // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
-    const diagram = $(go.Diagram, {
-      'undoManager.isEnabled': true, // must be set to allow for model change listening
-      // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
-      'clickCreatingTool.archetypeNodeData': {
-        text: 'new node',
-        color: 'lightblue',
-      },
-      model: new go.GraphLinksModel({
-        linkKeyProperty: 'key', // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
-      }),
-    });
-
-    // define a simple Node template
-    diagram.nodeTemplate = $(
-      go.Node,
-      'Auto', // the Shape will go around the TextBlock
-      new go.Binding('location', 'loc', go.Point.parse).makeTwoWay(
-        go.Point.stringify
-      ),
-      $(
-        go.Shape,
-        'RoundedRectangle',
-        { name: 'SHAPE', fill: 'white', strokeWidth: 0 },
-        // Shape.fill is bound to Node.data.color
-        new go.Binding('fill', 'color')
-      ),
-      $(
-        go.TextBlock,
-        { margin: 8, editable: true }, // some room around the text
-        new go.Binding('text').makeTwoWay()
-      )
-    );
-
-    return diagram;
+  function handleModelChange(changes: go.IncrementalData) {
+    console.log(changes);
   }
 
-  function handleModelChange() {}
-
   return (
-    <ReactDiagram
-      ref={ref}
-      initDiagram={initDiagram}
-      divClassName="chart"
+    <ChartWrapper
       nodeDataArray={MOCK_NODE_DATA}
       linkDataArray={MOCK_LINK_DATA}
       onModelChange={handleModelChange}
---
+++ src/ChartWrapper.tsx
@@ -0,0 +1,72 @@
+import { useRef } from 'react';
+import { ReactDiagram } from 'gojs-react';
+import go from 'gojs';
+
+interface Props {
+  nodeDataArray: Array<go.ObjectData>;
+  linkDataArray: Array<go.ObjectData>;
+  modelData?: go.ObjectData;
+  skipsDiagramUpdate?: boolean;
+  onDiagramEvent?: (e: go.DiagramEvent) => void;
+  onModelChange?: (e: go.IncrementalData) => void;
+}
+
+function ChartWrapper({
+  nodeDataArray = [],
+  linkDataArray = [],
+  onModelChange = () => undefined,
+}: Props) {
+  const ref = useRef(null);
+
+  function initDiagram() {
+    const $ = go.GraphObject.make;
+    // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
+    const diagram = $(go.Diagram, {
+      'undoManager.isEnabled': true, // must be set to allow for model change listening
+      // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
+      'clickCreatingTool.archetypeNodeData': {
+        text: 'new node',
+        color: 'lightblue',
+      },
+      model: new go.GraphLinksModel({
+        linkKeyProperty: 'key', // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
+      }),
+    });
+
+    // define a simple Node template
+    diagram.nodeTemplate = $(
+      go.Node,
+      'Auto', // the Shape will go around the TextBlock
+      new go.Binding('location', 'loc', go.Point.parse).makeTwoWay(
+        go.Point.stringify
+      ),
+      $(
+        go.Shape,
+        'RoundedRectangle',
+        { name: 'SHAPE', fill: 'white', strokeWidth: 0 },
+        // Shape.fill is bound to Node.data.color
+        new go.Binding('fill', 'color')
+      ),
+      $(
+        go.TextBlock,
+        { margin: 8, editable: true }, // some room around the text
+        new go.Binding('text').makeTwoWay()
+      )
+    );
+
+    return diagram;
+  }
+
+  return (
+    <ReactDiagram
+      ref={ref}
+      initDiagram={initDiagram}
+      divClassName="chart"
+      nodeDataArray={nodeDataArray}
+      linkDataArray={linkDataArray}
+      onModelChange={onModelChange}
+    />
+  );
+}
+
+export default ChartWrapper;
